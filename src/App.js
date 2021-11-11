c548a067f9cd389f4e997c544c699c8336056eec
Marina-93 <marina.ua93@gmail.com>
2021-11-11 23:48:23 +0200

    react feetback 2.3

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import Section from './components/Section';
-import FeedbackOptions from './components/FeedbackOptions';
-import Status from './components/Status';
-import Notification from './components/Notification';
+import Section from './components/Section/Section';
+import FeedbackOptions from './components/FeedbackOptions/FeedbackOptions';
+import Status from './components/Status/Status';
+import Notification from './components/Notification/Notification';
 
 class App extends React.Component{
   state = {
@@ -22,7 +22,7 @@ class App extends React.Component{
   }
 
   countPositiveFeedbackPercentage() {
-      return this.state.good === 0
+    return this.state.good === 0
     ? "0"
     : Math.round((this.state.good * 100) / this.countTotalFeedback()) + "%";
   }
@@ -30,22 +30,22 @@ class App extends React.Component{
   render() {
     return (
       <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions onLeaveFeedback={this.leaveFeedback.bind(this)} />
-        </Section>
-        <Section title="Statistics" />
-        {this.countTotalFeedback() ? (
-          <Status
-            good={this.state.good}
-            neutral={this.state.neutral}
-            bad={this.state.bad}
-            total={this.countTotalFeedback()}
-            positiveFeedback={this.countPositiveFeedbackPercentage()}
-          />
+      <Section title="Please leave feedback">
+      <FeedbackOptions onLeaveFeedback={this.leaveFeedback.bind(this)} />
+      </Section>
+      <Section title="Statistics" />
+      {this.countTotalFeedback() ? (
+        <Status
+        good={this.state.good}
+        neutral={this.state.neutral}
+        bad={this.state.bad}
+        total={this.countTotalFeedback()}
+        positiveFeedback={this.countPositiveFeedbackPercentage()}
+        />
         ) : (
           <Notification message="No feedback given"></Notification>
-        )}
-      </>
-        )}}
+          )}
+          </>
+          )}}
 
 export default App
--- src/components/Section/Section.js
+++ src/components/Section/Section.js
@@ -1,13 +1,14 @@
 import propTypes from "prop-types";
+import './Section.css'
 
 export default function Section({ title, children }) {
   return (
-    <section className="contaner">
-      <h1 >{title}</h1>
-      {children}
-    </section>
+  <section className="contaner">
+  <h1 >{title}</h1>
+  {children}
+  </section>
   )
-}
+  }
 
 Section.propTypes = {
   title: propTypes.string,
